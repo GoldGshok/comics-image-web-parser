8f250d84db80a3a83b86b4942d533c60a92db622
GoldGshok <goldgshok@gmail.com>
2023-04-07 18:06:12 +0700

    refactor

--- src/main/java/com/goldgshok/parser/request/ParameterRequest.java
+++ src/main/java/com/goldgshok/parser/request/ParameterRequest.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 public class ParameterRequest {
 
-    @Schema(description = "URL сайта", example = "https://mangapoisk.ru/manga/attack-on-titan-2/chapter/%s", required = true)
+    @Schema(description = "URL сайта", example = "https://mangapoisk.ru/manga/attack-on-titan-2/chapter/%s", requiredMode = Schema.RequiredMode.REQUIRED)
     private String url;
     @Schema(description = "Начальная страница для поиска картинок", example = "100", required = true)
     private Integer startPageNumber;
